1141a73392da8929609cd1117697691ab31c4a4e
taitddev <taitd.dev@gmail.com>
2022-08-20 21:39:49 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AuthContext";
 
 const SignUpPage = React.lazy(() => import("./pages/SignUp"));
-const LoginPage = React.lazy(() => import("./pages/Login"));
+// const LoginPage = React.lazy(() => import("./pages/Login"));
+
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -12,7 +14,7 @@ function App() {
         <Suspense>
           <Routes>
             <Route path="/signup" element={<SignUpPage />}></Route>
-            <Route path="/login" element={<LoginPage />}></Route>
+            <Route path="/login" element={<Login />}></Route>
           </Routes>
         </Suspense>
       </AuthContextProvider>
